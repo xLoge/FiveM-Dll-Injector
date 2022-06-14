bf2e3ed7c9aee9115633484213d60356881b4855
Loge <85043963+xLoge@users.noreply.github.com>
2022-06-14 14:29:33 +0200

    Update Injector.h

--- Project/Injector.h
+++ Project/Injector.h
@@ -1,57 +1,57 @@
 #pragma once
 
-void Injector(const char* filepath)
+void Injector(const char* filePath)
 {
-	DWORD procId = NULL;
+	DWORD procId = 0;
+	static int ms = 1;
+	bool once = 1;
 
-	bool IsWaiting = false;
+	while (!procId)
+	{		
+		CheckProc("FiveM_GameProcess.exe", procId, ms);
+		CheckProc("FiveM_b2060_GameProcess.exe", procId, ms);
+		CheckProc("FiveM_b2189_GameProcess.exe", procId, ms);
+		CheckProc("FiveM_b2372_GameProcess.exe", procId, ms);
+		CheckProc("FiveM_b2545_GameProcess.exe", procId, ms);
+		CheckProc("FiveM_GTAProcess.exe", procId, ms);
+		CheckProc("FiveM_b2060_GTAProcess.exe", procId, ms);
+		CheckProc("FiveM_b2189_GTAProcess.exe", procId, ms);
+		CheckProc("FiveM_b2372_GTAProcess.exe", procId, ms);
+		CheckProc("FiveM_b2545_GTAProcess.exe", procId, ms);
 
-	while (true)
-	{
-		CheckProc("FiveM_GameProcess.exe", procId);
-		CheckProc("FiveM_b2060_GameProcess.exe", procId);
-		CheckProc("FiveM_b2189_GameProcess.exe", procId);
-		CheckProc("FiveM_b2372_GameProcess.exe", procId);
-		CheckProc("FiveM_b2545_GameProcess.exe", procId);
-
-		CheckProc("FiveM_GTAProcess.exe", procId);
-		CheckProc("FiveM_b2060_GTAProcess.exe", procId);
-		CheckProc("FiveM_b2189_GTAProcess.exe", procId);
-		CheckProc("FiveM_b2372_GTAProcess.exe", procId);
-		CheckProc("FiveM_b2545_GTAProcess.exe", procId);
-
-		if (procId) break;
-
-		if (!IsWaiting)
+		if (once)
 		{
-			std::cout << "Waiting for FiveM ...";
-			IsWaiting = true;
+			if (procId)
+				break;
+			else
+			{
+				std::cout << "File: \"" << filePath << "\"\n\n";
+				std::cout << "Waiting for FiveM ...";
+				ms = 30;
+				once = false;
+			}
 		}
 	}
 
-	if (IsWaiting)
-	{
-		std::cout << "\nFound FiveM! (PID: " << procId << ") Waiting 5 Seconds to prevent errors";
-		Sleep(5000);
-	}
+	if (!once)
+		std::cout << "\nFound FiveM! (PID: " << procId << ") Waiting 8 Seconds to prevent errors."; Sleep(8000);
 
 	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, false, procId);
 
-	if (hProc != INVALID_HANDLE_VALUE)
+	if (hProc && hProc != INVALID_HANDLE_VALUE)
 	{
 		void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
 		if (loc)
-			WriteProcessMemory(hProc, loc, filepath, strlen(filepath) + 1, 0);
+			WriteProcessMemory(hProc, loc, filePath, strlen(filePath) + 1, 0);
 
 		HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
 
 		if (hThread)
 			CloseHandle(hThread);
 	}
-
 	if (hProc)
 		CloseHandle(hProc);
-
+	
 	return;
 }
